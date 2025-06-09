e34ddc925a68879b6a1f14f2f8c127c032c8d315
Priyanshu Agrawal <priyanshuagrawal@outlook.com>
2025-06-09 22:41:02 +0530

    Initial Commit

---
+++ data/projectsData.ts
@@ -0,0 +1,25 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Bing.com',
+    description: `Implemented user authentication for bing.com using OpenIdConnect.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.bing.com',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+]
+
+export default projectsData
